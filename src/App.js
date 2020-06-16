122e67f6bd1e38ec1c30cffc7af15e34b9b8b8df
mrfleming <ericfleming8@gmail.com>
2020-06-15 21:54:15 -0500

    def. state, fn's/routes, useEffect, return JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
+import Recipe from './Recipe';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID_EDAMAM = process.env.REACT_APP_EDAMAM_APP_ID;
+  const API_KEY_EDAMAM = process.env.REACT_APP_EDAMAM_API_KEY;
+
+
+
+
+
+  const [counter, setCounter] = useState(0);//pass in default value
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID_EDAMAM}&app_key=${API_KEY_EDAMAM}`);
+    const data = await response.json();
+    console.log(data.hits);
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = (event) => {
+    setSearch(event.target.value);
+    console.log(search);
+  };
+
+  const fetchQuery = (event) => {
+    event.preventDefault();
+    setQuery(search);
+  }
+
+
+
+  useEffect(()=>{
+    console.log("An effect has taken place...");
+    getRecipes();
+  }, [query]);
+  // the second parameter only runs once on page load if empty
+  // adding values in the array will update when the value changes
+
+
+  
+
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React Recipes</h1>
+      <form onSubmit={fetchQuery} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}></input>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <div>
+        <h2 onClick={() => setCounter(counter+1)}>{counter}</h2>
+      </div>
+      <div className="myrecipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 }
